d91715f2b821966e0e1c23dcc068c0a63499f8b0
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-18 05:16:48 +0300

    It scans the desired category, the number of pages has become automated, minor bugs have been fixed. It's completely finished.

--- cimri_scrapy/cimri.py
+++ cimri_scrapy/cimri.py
@@ -5,9 +5,16 @@ from datetime import datetime
 class CimriSpider(scrapy.Spider):
     number_items_received = -32
     name = 'cimri'
-    start_gazli_icecek = 1
-    start_atistirmalik = 1
-    start_urls = [f'https://www.cimri.com/market/gazli-icecek?page={start_gazli_icecek}']
+    start_category = 1
+    
+    category = str(input("""
+                         Type the category to be scanned.
+                         Categories
+                         ************
+                         (gida,sut-ve-kahvaltilik,meyve-ve-sebze,icecek,et-tavuk-ve-balik,deterjan-ve-temizlik-urunleri)
+                         : """))
+    
+    start_urls = [f'https://www.cimri.com/market/{category}?page={start_category}']
     
     csv_data = []
 
@@ -15,52 +22,10 @@ class CimriSpider(scrapy.Spider):
         current_day = datetime.now()
         formatted_date = current_day.strftime("%d.%m.%Y")
 
-        category_name = "Gazli_Icecek"
+        category_name = self.category
         product_title = response.css(".ProductCard_productName__35zi5 p::text").extract()
-        product_price = response.css(".ProductCard_price__10UHp::text").extract()
-        # To regulate the excess in the price of the product
-        product_price = [elem for i, elem in enumerate(product_price) if i == 0 or i == len(product_title) - 1 or i % 2 == 0]
-        
-        # Pulling the brand name and edit the list
-        brand = response.css(".WrapperBox_small__VfD9r img").xpath("@alt").extract()
-        brand = [i for i in brand if i != '']
-        brand = [elem for i, elem in enumerate(brand) if i < 4 or i % 2 == 0]
-        
-        # Getting the product link and the real link
-        links = response.css("div.ProductCard_productCard__412iI a::attr(href)").extract()
-        cimri_link = []
-        real_link = []
-        # Parsing all links as cimri and real links
-        for i, item in enumerate(links):
-            if i % 2 == 0:
-                cimri_link.append('https://www.cimri.com'+item)
-            else:
-                real_link.append(item)
-        
-        # Exporting all products on the page to csv_data
-        for i in range(0,len(product_title)):
-            self.csv_data.append([formatted_date,category_name,brand[i],product_title[i],product_price[i],cimri_link[i],real_link[i]])
-        
-        self.start_gazli_icecek += 1
-        next_page_gazli_icecek = f'https://www.cimri.com/market/gazli-icecek?page={self.start_gazli_icecek}'
-        
-        # The yield part until all pages are finished
-        if(len(product_title) > 30):
-            self.number_items_received += 32
-            print('Number of products taken from this Page:',len(self.csv_data)-self.number_items_received)
-            print('Total number of lines:',len(self.csv_data))
-            yield response.follow(url = next_page_gazli_icecek, callback = self.parse, dont_filter=True)
-        else:
-            self.number_items_received += len(product_title)
-            yield response.follow(url = 'https://www.cimri.com/market/atistirmalik?page=1', callback = self.product_parse, dont_filter=True)
-    
-    # Moving to the snack part 
-    def product_parse(self, response):
-        current_day = datetime.now()
-        formatted_date = current_day.strftime("%d.%m.%Y")
-
-        category_name = "atistirmalik"
-        product_title = response.css(".ProductCard_productName__35zi5 p::text").extract()
+        product_price = response.css('.ProductCard_footer__Fc9OL .ProductCard_price__10UHp::text').extract()
+        """
         product_price = response.css(".ProductCard_price__10UHp::text").extract()
         # Some pages do not write the product price, and the slippage was checked on the pages that do not write the product price.
         if(len(product_price) == 64):
@@ -76,6 +41,7 @@ class CimriSpider(scrapy.Spider):
                 previous = item
             product_price.clear()
             product_price.extend(product_price2)
+        """
             
         brand = response.css(".WrapperBox_small__VfD9r img").xpath("@alt").extract()
         brand = [i for i in brand if i != '']
@@ -92,22 +58,26 @@ class CimriSpider(scrapy.Spider):
             else:
                 real_link.append(item)
         # Exporting all products on the page to csv_data
+        print(len(brand),len(product_title),len(product_price),len(cimri_link),len(real_link))
         for i in range(0,len(product_title)):
             self.csv_data.append([formatted_date,category_name,brand[i],product_title[i],product_price[i],cimri_link[i],real_link[i]])
         
-        self.start_atistirmalik += 1
-        next_page_atistirmalik = f'https://www.cimri.com/market/atistirmalik?page={self.start_atistirmalik}'
+        self.start_category += 1
+        next_page_atistirmalik = f'https://www.cimri.com/market/{self.category}?page={self.start_category}'
+        
+        #We converted the text value of the last button that shows the page numbers below to int.
+        page_number = int(response.css("div.Pagination_pagination__6kvLO li a::text").extract()[-1])
         
         # The yield part until all pages are finished
-        if(self.start_atistirmalik < 51):
+        if(self.start_category <= page_number):
             self.number_items_received += 32
             print('Number of products taken from this Page:',len(self.csv_data)-self.number_items_received)
             print('Total number of lines:',len(self.csv_data))
-            yield response.follow(url = next_page_atistirmalik, callback = self.product_parse, dont_filter=True)
+            yield response.follow(url = next_page_atistirmalik, callback = self.parse, dont_filter=True)
         else:
             # Exporting csv_data to excel when all pages are finished
             self.number_items_received += len(product_title)
             self.df = pd.DataFrame(self.csv_data)
             self.df.columns = ["Date","Category_name","Brand","Product_title","Product_price","cimri_link","Real_link"]
-            self.df.to_excel("Urunler.xlsx")
+            self.df.to_excel(f"Products_of_{self.category}_{formatted_date}.xlsx")
         
