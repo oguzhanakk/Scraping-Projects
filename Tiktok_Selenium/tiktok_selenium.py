bcd860aa473465b3c7410236dbe704d3ae961152
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-20 11:41:45 +0300

    general structure was created.

--- Tiktok_Selenium/tiktok_selenium.py
+++ Tiktok_Selenium/tiktok_selenium.py
@@ -46,21 +46,43 @@ def country_selection(browser,location,sayi):
     time.sleep(2)
     try_except_click(browser, '//*[@id="ccContentContainer"]/div[2]/div/div[3]/div/div[1]/div')
     time.sleep(2)
+    
+def time_selection(browser,time='7 days',functions='5'):
+    if(functions == 1):
+        try_except_click(browser, '//*[@id="hashtagPeriodSelect"]/span/div/div/div')
+        if(time == '30 days'):
+            try_except_click(browser, '/html/body/div[5]/div/div/div/div/div/div[2]')
+        elif(time == '120 days'):
+            try_except_click(browser, '/html/body/div[5]/div/div/div/div/div/div[3]')
+    if(functions == 2):
+        try_except_click(browser, '//*[@id="soundPeriodSelect"]/span/div/div/div')
+        if(time == '30 days'):
+            try_except_click(browser, '/html/body/div[6]/div/div/div/div/div/div[2]')
+        elif(time == '120 days'):
+            try_except_click(browser, '/html/body/div[5]/div/div/div/div/div/div[3]')
+    if(functions == 3):
+        pass
+        #try_except_click(browser, '//*[@id="creatorFansRegionSelect"]/span/div/div/div')
+    if(functions == 4):
+        try_except_click(browser, '//*[@id="tiktokPeriodSelect"]/span/div/div/div')
+        if(time == '30 days'):
+            try_except_click(browser, '/html/body/div[5]/div/div/div/div/div/div[2]')
+    
 
 #--------------------------------------------------------------------------------------------------------------------------------
 
-def Hashtags_info(browser,location):
+def Hashtags_info(browser,location='None'):
     
     browser.get('https://ads.tiktok.com/business/creativecenter/inspiration/popular/hashtag/pc/en')
     
-    country_selection(browser,location,'66')
+    if(location != 'None'):
+        country_selection(browser,location,'66')
     
     first_div = '.list-wrap--DmWKY.list-wrap---Aj1L.cards--p6uqu.cards--dxZdb'
     second_div = '.card-wrapper--o1aWr.card-wrapper--RF-4I'
     
     length = browser.find_element(By.CSS_SELECTOR, first_div).find_elements(By.CSS_SELECTOR, second_div)
 
-    
     rank = try_except_getinfo(browser,len(length),first_div,second_div,'.rankingIndex--CRstI.rankingIndex--d5sdy')
     hashtag = try_except_getinfo(browser,len(length),first_div,second_div,'.titleText--qKHbP')
     posts = try_except_getinfo(browser,len(length),first_div,second_div,'.item-wrapper--gXjma')
@@ -70,33 +92,44 @@ def Hashtags_info(browser,location):
     for i in range(0,len(length)):
         id = browser.find_element(By.CSS_SELECTOR,first_div).find_elements(By.CSS_SELECTOR,second_div)[i].find_elements(By.CSS_SELECTOR,'.item-wrapper--gXjma')[1]
         Views.append(id.text)
+    print(Views)
                     
-    print(rank,hashtag,posts,Views)
+    #print(rank,hashtag,posts,Views)
+    
+    hashtags_info = []
+    for i in range(0,len(rank)):
+        hashtags_info.append([rank[i],hashtag[i],posts[i],Views[i]])
 
-def Songs_info(browser,location):
+def Songs_info(browser,location="None"):
     
     browser.get('https://ads.tiktok.com/business/creativecenter/inspiration/popular/music/pc/en')
     
-    country_selection(browser,location,'65')
+    if(location != 'None'):
+        country_selection(browser,location,'65')
     
     first_div = '.list-wrap--DmWKY.list-wrap---Aj1L.sound-list-wrapper--Srct8'
     second_div = '.card-wrapper--o1aWr.card-wrapper--RF-4I'
     
     length = browser.find_element(By.CSS_SELECTOR, first_div).find_elements(By.CSS_SELECTOR, second_div)
 
-    
     rank = try_except_getinfo(browser,len(length),first_div,second_div,'.rankingIndex--CRstI.rankingIndex--d5sdy')
     rank_change = try_except_getinfo(browser,len(length),first_div,second_div,'.rankingvalue--yOyah.rankingvalue--0zzQp.rising--1S40D.rising--XSUMU')
     music_name = try_except_getinfo(browser,len(length),first_div,second_div,'.music-name--Z2hNc.music-name--G2iqZ')
     author_name = try_except_getinfo(browser,len(length),first_div,second_div,'.auther-name--3HglG.auther-name--cXfro')
     
-    print(rank,rank_change,music_name,author_name)
+    #print(rank,rank_change,music_name,author_name)
     
-def Creators_info(browser,location):
+    songs_info = []
+    for i in range(0,len(rank)):
+        songs_info.append([rank[i],rank_change[i],music_name[i],author_name[i]])
+    print(songs_info)
+    
+def Creators_info(browser,location='None'):
     
     browser.get('https://ads.tiktok.com/business/creativecenter/inspiration/popular/creator/pc/en')
     
-    country_selection(browser,location,'20')
+    if(location != 'None'):
+        country_selection(browser,location,'20')
     
     #Audience region'u istedigine çevirmen lazım.
     
@@ -114,33 +147,42 @@ def Creators_info(browser,location):
         id = browser.find_element(By.CSS_SELECTOR,first_div).find_elements(By.CSS_SELECTOR,second_div)[i].find_elements(By.CSS_SELECTOR,'.creator-data-wrap--m07J5')[1]
         likes.append(id.text)
     
-    videos_likes = []
+    videos_likes_original = []
     for i in range(0,len(length)):
         for j in range(0,3):
             id = browser.find_element(By.CSS_SELECTOR,first_div).find_elements(By.CSS_SELECTOR,second_div)[i].find_elements(By.CSS_SELECTOR,'.videos-item--cgrgr.videos-item--TmyQS')[j]
-            videos_likes.append(id.text)
+            videos_likes_original.append(id.text)
+    videos_likes = []
+    for i in range(0, len(videos_likes_original), 3):
+        videos_likes.append(videos_likes_original[i:i+3])
     
-    print(Music_name,posts,likes,videos_likes)
+    #print(Music_name,posts,likes,videos_likes)
+    creators_info = []
+    for i in range(0,len(Music_name)):
+        creators_info.append([Music_name[i],posts[i],likes[i],videos_likes[i]])
+    print(creators_info)
     
-def TikTok_Videos_info(browser,location):
+def TikTok_Videos_info(browser,location='None'):
     
     browser.get('https://ads.tiktok.com/business/creativecenter/inspiration/popular/pc/en')
     
-    country_selection(browser,location,'21')
+    if(location != 'None'):
+        country_selection(browser,location,'21')
     
     first_div = '.list-wrap--DmWKY.list-wrap---Aj1L'
     second_div = '.card-wrapper--o1aWr.card-wrapper--RF-4I.card-wrapper--JTsVL.card-wrapper--rQplW'
     
     length = browser.find_element(By.CSS_SELECTOR, first_div).find_elements(By.CSS_SELECTOR, second_div)
     
-    links = try_except_getAhref(browser,len(length),first_div,second_div,'.poster--FY5c7.poster--7inWR a')
+    links = try_except_getinfo(browser,len(length),first_div,second_div,'.poster--FY5c7.poster--7inWR a',ahref=2)
     
     print(links)
     
 
 def main():
     
-    browser = webdriver.Chrome()  
+    browser = webdriver.Chrome()
+    browser.maximize_window()  
 
     """
     location = str(input('''
@@ -154,11 +196,11 @@ def main():
                                 :'''))
     """
     #Functions
-    #Hashtags_info(browser,'Turkey')
-    #time.sleep(5)
-    #Songs_info(browser, 'Turkey')
-    #time.sleep(5)
-    #Creators_info(browser,'Turkey')
+    Hashtags_info(browser,'Turkey')
+    time.sleep(5)
+    Songs_info(browser, 'Turkey')
+    time.sleep(5)
+    Creators_info(browser,'Turkey')
     time.sleep(5)
     TikTok_Videos_info(browser,'Turkey')
     
