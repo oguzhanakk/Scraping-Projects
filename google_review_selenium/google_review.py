2b7357618aa57f6d1133595b87ee253a11a0075c
Oğuzhan Akkoyunlu <Oguzhanakkoyun1@outlook.com>
2023-02-21 15:21:28 +0300

    update

--- google_review_selenium/google_review.py
+++ google_review_selenium/google_review.py
@@ -3,6 +3,14 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+HOST = os.getenv("HOST")
+DATABASE = os.getenv("DATABASE")   
+USER = os.getenv("USER")   
+PASSWORD = os.getenv("PASSWORD")   
+PORT = os.getenv("PORT")
 
 def try_except_getinfo(browser,length,first_div,second_div,third_div,ahref=1,attribute='href'):
     list = []
@@ -26,22 +34,28 @@ def try_except_getinfo(browser,length,first_div,second_div,third_div,ahref=1,att
         print(f"{third_div} is done.") #This function is complete
         return(list)
 
-def show_more_and_scroll(browser,scroll_number):
+def show_more_and_scroll(browser):
+    
+    #document.querySelector('.jANrlb').querySelector('.fontBodySmall').textContent
+    elements = browser.find_element(By.CSS_SELECTOR,'.jANrlb').find_element(By.CSS_SELECTOR,'.fontBodySmall')
+    numeric_part = ''.join(filter(str.isdigit, elements.text))
+    scroll_number = int(float(numeric_part) / 10)
     
     #Scroll
     scroll_script = """
     const myScrollDiv = document.querySelector('.m6QErb.DxyBCb.kA9KIf.dS8AEf');
     myScrollDiv.scrollTo(0, myScrollDiv.scrollHeight);
     """
-    for i in range(0,scroll_number): #range(0,variable) is given manually and needs to be automated. !!!
+    for i in range(0,scroll_number+20): #range(0,variable) is given manually and needs to be automated. !!!
         browser.execute_script(scroll_script)
         time.sleep(1)
     time.sleep(1)
+    print("Scroll is done.")
     
     #show more
     while True:
         try:
-            button = browser.find_elements(By.CSS_SELECTOR, 'button[aria-label="Daha fazla göster"]')
+            button = browser.find_elements(By.CSS_SELECTOR, 'button[aria-label=" See more "]')
             for i in range(0,len(button)):
                 button[i].click()
                 time.sleep(1)
@@ -72,10 +86,12 @@ def scan_the_site(browser):
     comment = []
     for i in range(0,len(comment_2)):
         try:
-            result = comment_2[i].split("(Orijinal)")[1]
+            result = comment_2[i].split("(Original)")[1]
             comment.append(result)
+            print(result)
         except:
             comment.append(comment_2[i])
+
     given_star = try_except_getinfo(browser,length,first_div,second_div,'.kvMYJc',ahref=2,attribute='aria-label')
     
     all_csv = []
@@ -91,12 +107,21 @@ def scan_the_site(browser):
     
     #print(f"{excel_name} created.")
     
-def postgre_insert(df, excel_name):
-        
+def postgre_insert(df, links):
+    
+    start_index = links.find("place/") + len("place/")
+    end_index = links.find("/", start_index)
+
+    restaurant_name = links[start_index:end_index]
+    restaurant_name = restaurant_name.replace("'","")
+    restaurant_name = restaurant_name.replace("+","_")
+    restaurant_name = restaurant_name.replace("-","_")
+    print(restaurant_name)
+       
     conn = None
     # Connect to the database
     print("before connection")
-    conn = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='10suzolmaz', port='5432')
+    conn = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
     print("after connection")
     print("connection is created")
 
@@ -109,13 +134,13 @@ def postgre_insert(df, excel_name):
 
     #...
     # Create table
-    cur.execute(f'''CREATE TABLE {excel_name}
-                (User_Name text, User_info text, Given_Star text, 
+    cur.execute(f'''CREATE TABLE IF NOT EXISTS Google_map_reviews
+                (Restaurant_name text, User_Name text, User_info text, Given_Star text, 
                     Comment_time text, Comment text)''')
     
     # Add trends to db
     for i, row in df.iterrows():
-        cur.execute(f"INSERT INTO {excel_name} (User_Name, User_info, Given_Star, Comment_time, Comment) VALUES (%s, %s, %s, %s, %s)", (row['User_Name'], row['User_info'], row['Given_Star'], row['Comment_time'], row['Comment']))
+        cur.execute(f"INSERT INTO Google_map_reviews (Restaurant_name, User_Name, User_info, Given_Star, Comment_time, Comment) VALUES (%s, %s, %s, %s, %s, %s)", (restaurant_name, row['User_Name'], row['User_info'], row['Given_Star'], row['Comment_time'], row['Comment']))
     print("data is inserted to specified db on postgre sql")
 
     # Commit the changes to the database
@@ -134,19 +159,17 @@ def main():
     
     #Can change to the link,scroll_number,excel_name
     #75 scroll ends 679 comments.Each scroll is like 10 comments.
-    links = ["https://www.google.com/maps/place/Domino's+Pizza/@24.7927019,54.4386021,9z/data=!4m8!3m7!1s0x3e5f5b95260a5bfd:0xcdcff17606bf5004!8m2!3d25.3258811!4d55.3798685!9m1!1b1!16s%2Fg%2F1hc7h4hld"]
-    scroll_numbers = [200]
-    excel_name = ["Buhaira_Oasis_Tower_Sharjah_Dominos"]
+    links = ["https://www.google.com/maps/place/800PIZZA+Al+Barsha+-+Dubai/@24.8048997,54.5168008,10z/data=!4m8!3m7!1s0x3e5f682f20844969:0xb77679db1b7276f2!8m2!3d25.1161995!4d55.1949154!9m1!1b1!16s%2Fg%2F1vnrp2c6?hl=ehttps://www.google.com/maps/place/800PIZZA+Al+Barsha+-+Dubai/@24.8048997,54.5168008,10z/data=!4m8!3m7!1s0x3e5f682f20844969:0xb77679db1b7276f2!8m2!3d25.1161995!4d55.1949154!9m1!1b1!16s%2Fg%2F1vnrp2c6?hl=en"]
     
     for i in range(0,len(links)):
         browser.get(links[i])
         time.sleep(5)
 
-        show_more_and_scroll(browser,scroll_numbers[i])
+        show_more_and_scroll(browser)
         time.sleep(1)
         df = scan_the_site(browser)
         
-        postgre_insert(df,excel_name[i])
+        postgre_insert(df,links[i])
         
         time.sleep(3)
     
